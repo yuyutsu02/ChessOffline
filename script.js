7317c879d9edd74ff3c92370f4e9713c46d6ccb2
yuyutsu singh <yuyutsu02@gmail.com>
2024-01-20 21:57:16 +0530

    front page done

--- script.js
+++ script.js
@@ -13,17 +13,20 @@ const play=document.querySelector('.play');
 play.addEventListener('click',game_start);
 
 
+
+//function to take game info and direct to board
 function game_start(){
     const minutes=document.querySelector('.minutes');
     const increment=document.querySelector('.increment');
-
     if(minutes.value && increment.value ){
+
         console.log(`match of ${minutes.value}-minutes with ${increment.value}-seconds increment has been started`);
         localStorage.setItem('minutes',minutes.value);
         localStorage.setItem('increment',increment.value);
         minutes.value='';   
         increment.value='';   
         window.location.href = "../board/index.html";
+
     }else{
         alert(`invalid time>>- ${minutes.value}-minutes , ${increment.value}-increment`);
     }
